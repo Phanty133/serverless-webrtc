e362bb92b7f1df3559dbbee1d4afdb7205912d68
Phanty133 <48225206+Phanty133@users.noreply.github.com>
2022-12-07 12:23:31 +0200

    Cleaned up root directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require("path");
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
-	entry: './src/index.ts',
-	devtool: 'inline-source-map',
+	entry: "./src/index.ts",
+	devtool: "inline-source-map",
 	mode: "development",
 	watch: true,
 	cache: {
-		type: 'filesystem'
+		type: "filesystem"
 	},
 	module: {
 		rules: [
 			{
 				test: /\.tsx?$/,
-				use: 'swc-loader',
-				exclude: /node_modules/,
-			},
-		],
+				use: "swc-loader",
+				exclude: /node_modules/
+			}
+		]
 	},
 	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
+		extensions: [".tsx", ".ts", ".js"]
 	},
 	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'build'),
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "build")
 	},
 	plugins: [
 		new NodePolyfillPlugin()
